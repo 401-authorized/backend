cc9e06ca9b3cdfb6a1c91af2e85e3c30b2b6b390
Md Tanwir Ahmad <58876236+tanwir786@users.noreply.github.com>
2022-02-14 11:55:33 +0530

    Update hr.route.js

--- src/routes/hr.route.js
+++ src/routes/hr.route.js
@@ -1,5 +1,28 @@
-const { getAllHr } = require("../controllers/hr.controller");
+const express = require("express");
+const router = express.router();
 
-const router = require("express").Router();
+const JNF = require('../models/jnf.model');
 
-router.get("/", getAllHr);
+router.get('/login' ,(req,res)=>{
+res.send('login');
+})
+
+router.post('/login',(req,res)=>{
+    res.send('logged in');
+})
+
+router.get('/register',(req,res)=>{
+    res.send('sign up form');
+})
+
+router.post('/register',(req,res)=>{
+    res.send('registered');
+})
+
+router.get('/dashboard',async (req,res)=>{
+const {id} = req.params;
+const jnfs = await JNF.findById(id);
+
+
+})
+
