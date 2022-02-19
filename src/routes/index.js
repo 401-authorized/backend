d71df8056d41dd9b862c3902341551bd752a6cd4
Kushak Jafry <kushakjafry@gmail.com>
2022-02-19 10:30:14 +0530

    added all the route deefinition in ./routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,12 @@ const router = require("express").Router();
 const infRouter = require("./inf.route");
 const oauthRouter = require("./oauth.route");
 const mailRouter = require("./mail.route");
+const hrRouter = require("./hr.route");
+const invitationRouter = require("./invitation.route");
 router.use("/inf", infRouter);
 router.use("/oauth", oauthRouter);
 router.use("/mail", mailRouter);
+router.use("/hr", hrRouter);
+router.use("/invitation", invitationRouter);
 
 module.exports = router;
