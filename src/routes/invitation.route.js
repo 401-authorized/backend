eeba0a5db93e2a8ad75b031d586b82ce30c1d949
Kushak Jafry <kushakjafry@gmail.com>
2022-03-01 15:51:21 +0530

    some changes

--- src/routes/invitation.route.js
+++ src/routes/invitation.route.js
@@ -6,9 +6,8 @@ const Invitation = require("../models/invitation.model");
 const auth = require("../utils/auth");
 const IndulgeExceptionHandler = require("../core/IndulgeExceptionHandler");
 const IndulgeInternalServerException = require("../exceptions/IndulgeInternalServerException");
-const {templates}= require("../utils/templates");
+const { templates } = require("../utils/templates");
 const { sendMail } = require("../utils/mail");
-
 
 const generateJWT = (company) => {
   try {
@@ -29,25 +28,24 @@ const generateJWT = (company) => {
 
 router.post("/", auth.authenticate, auth.verifyAdmin, async (req, res) => {
   try {
-    const { name ,email} = req.body;
-    let company = await Company.findOne({ name});
-    if(!company)
-    {
-      company = new Company({name});
+    const { name, email } = req.body;
+    let company = await Company.findOne({ name });
+    if (!company) {
+      company = new Company({ name });
       await company.save();
     }
     const token = generateJWT(company);
     const newInvitation = new Invitation({
       companyId: company._id,
       token,
-      email
+      email,
     });
     await newInvitation.save();
-    const url = `https://localhost:3000/signup/?code=${token}`;
-    await sendMail(templates.INVITATION,{name,url},email);
+    const url = `${process.env.BASE_URL}signup?code=${token}`;
+    await sendMail(templates.INVITATION, { name, url }, email);
     res.send({
-        success: true,
-        invitation: newInvitation
+      success: true,
+      invitation: newInvitation,
     });
   } catch (err) {
     const e = IndulgeExceptionHandler(err);
